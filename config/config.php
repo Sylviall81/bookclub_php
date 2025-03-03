6563c1df9cb3aa366ab4f52b8c9ff075d33f54e7
Sylvia Llorente <sylviall81@gmail.com>
2025-03-03 10:40:06 +0100

    replace config

--- config/config.php
+++ config/config.php
@@ -1,15 +1,11 @@
 <?php
 // Configuración general
 define('SITE_NAME', 'The Book Club');
-define('BASE_URL', 'http://localhost/httpdocs/bookclub/');//hay q reemplazar con esto?http://www.myphpproject.net.mialias.net/index.php
-
-// Configuración de la base de datos
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'bookclubdb');
-define('DB_USER', 'root');
-define('DB_PASS', '45960967Kk*');
-//define('DB_PORT', '3306'); // Especifica el puerto
-
+define('BASE_URL', 'https://app-828c7e83-e0b0-41c5-855f-c3cf129aecf1.cleverapps.io/');
+define('DB_HOST', 'b1rqdpwocrarevqbq2qs-mysql.services.clever-cloud.com'); //reemplazar con datos phpadmin
+define('DB_NAME', 'b1rqdpwocrarevqbq2qs');
+define('DB_USER', 'u6uds9yt2hhhbnhs');
+define('DB_PASS', 'Vx2xIg0KSTec57MTmDPX');
 
 
 $referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : BASE_URL.'/public';
